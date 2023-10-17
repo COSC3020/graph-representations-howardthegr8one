a4e129d211b29cbd1dbb0a0f80f83b2d0096d496
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2023-10-17 17:11:59 -0600

    Update code.js
    converted my code to use 2d array instead of dictionary of arrays

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
-function convertToAdjMatrix(adjList) {
-    return adjList;
+
+function convertToAdjMatrix(graph) {
+    var numNodes = graph.length;
+    // initialize matrix values with zeros
+    var matrix = new Array(numNodes).fill(0).map(() => new Array(numNodes).fill(0));
+
+    var index = -1;
+    // iteratively replaces 0s with 1s for corresponding edges in matrix
+	for (var i = 0; i < graph.length; i++) {
+        index++;
+		for (var j = 0; j < graph[i].length; j++) {
+            var curr = graph[i][j];
+            if (index != curr)
+                matrix[index][curr] = 1;
+        }
+    }
+    return matrix;
 }
